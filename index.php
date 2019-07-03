c4d0d1a1f9ac7c13abdb0d14e3dde91517fc0e06
Fitra Fadilana <fitra90@gmail.com>
2019-07-03 17:21:31 +0700

    fixing blob show

--- index.php
+++ index.php
@@ -169,19 +169,21 @@ if (isset($_POST['SubmitButton'])) { //check if form was submitted
         //Upload blob
         // $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
         try {
-        
+
             $fileToUpload = $_FILES['berkas']['name'];
-        $content = fopen($_FILES['berkas']['tmp_name'], "r");
+            $content = fopen($_FILES['berkas']['tmp_name'], "r");
 
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+            $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 
-        // $type = $_FILES['berkas']['type'];
-        // $blob_name = $_FILES['berkas']['name'];
-         // Get blob.
-         echo "This is the content of the blob uploaded: ";
-         $blob = $blobClient->getBlob($containerName, $fileToUpload);
-         fpassthru($blob->getContentStream());
-         echo "<br />";
+            // $type = $_FILES['berkas']['type'];
+            // $blob_name = $_FILES['berkas']['name'];
+            // Get blob.
+            echo "This is the content of the blob uploaded: ";
+            $blob = $blobClient->getBlob($containerName, $fileToUpload);
+            header("Content-Type:".$_FILES['berkas']['type']);
+            header('Content-Disposition: attachment; filename="' . $blob_name . '"');
+            fpassthru($blob->getContentStream());
+            echo "<br />";
 
         } catch (ServiceException $e) {
             $code = $e->getCode();
