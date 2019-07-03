7699bfcf0d743f3f7e7204c0f00a82531edc0740
Fitra Fadilana <fitra90@gmail.com>
2019-07-03 13:33:33 +0700

    test upload 2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
- 
+
 <script type="text/javascript">
 
 <form></form>
@@ -14,10 +14,10 @@
         // **********************************************
         // *** Update or verify the following values. ***
         // **********************************************
- 
+
         // Replace <Subscription Key> with your valid subscription key.
         var subscriptionKey = "d7b3fef1dd844d2bbcc479775589001d";
- 
+
         // You must use the same Azure region in your REST API method as you used to
         // get your subscription keys. For example, if you got your subscription keys
         // from the West US region, replace "westcentralus" in the URL
@@ -29,40 +29,40 @@
         var uriBase =
             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
             // "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
- 
+
         // Request parameters.
         var params = {
             "visualFeatures": "Categories,Description,Color",
             "details": "",
             "language": "en",
         };
- 
+
         // Display the image.
         var sourceImageUrl = document.getElementById("inputImage").value;
         document.querySelector("#sourceImage").src = sourceImageUrl;
- 
+
         // Make the REST API call.
         $.ajax({
             url: uriBase + "?" + $.param(params),
- 
+
             // Request headers.
             beforeSend: function(xhrObj){
                 xhrObj.setRequestHeader("Content-Type","application/json");
                 xhrObj.setRequestHeader(
                     "Ocp-Apim-Subscription-Key", subscriptionKey);
             },
- 
+
             type: "POST",
- 
+
             // Request body.
             data: '{"url": ' + '"' + sourceImageUrl + '"}',
         })
- 
+
         .done(function(data) {
             // Show formatted JSON on webpage.
             $("#responseTextArea").val(JSON.stringify(data, null, 2));
         })
- 
+
         .fail(function(jqXHR, textStatus, errorThrown) {
             // Display error message.
             var errorString = (errorThrown === "") ? "Error. " :
@@ -73,7 +73,7 @@
         });
     };
 </script>
- 
+
 <h1>Analyze image:</h1>
 Upload an image to be analyzed!.
 <br><br>
@@ -101,7 +101,6 @@ Image to analyze:
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=<, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
@@ -129,75 +128,94 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=blibblob2;Accoun
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
-$fileToUpload = $_FILES['berkas'];
-
 if (isset($_POST['SubmitButton'])) { //check if form was submitted
-    // echo "<pre>";
-    // print_r($_FILES);
-    // echo "</pre>";
 
-    $createContainerOptions = new CreateContainerOptions();
+    $fileToUpload = '';
+    //UPLOAD FILES
+    if (($_FILES['berkas']['name'] != "")) {
+        // Where the file is going to be stored
+        $target_dir = "upload/";
+        $file = $_FILES['berkas']['name'];
+        $path = pathinfo($file);
+        $filename = $path['filename'];
+        $ext = $path['extension'];
+        $temp_name = $_FILES['berkas']['tmp_name'];
+        $path_filename_ext = $target_dir . $filename . "." . $ext;
 
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+        // Check if file already exists
+        if (file_exists($path_filename_ext)) {
+            echo "Sorry, file already exists.";
+        } else {
+            //MOVE FILES TO PROJECT UPLOAD DIRECTORY
+            move_uploaded_file($temp_name, $path_filename_ext);
+            echo "Congratulations! File Uploaded to Project Directory.";
 
-    // Set container metadata.
-    $createContainerOptions->addMetaData("key1", "value1");
-    $createContainerOptions->addMetaData("key2", "value2");
+            $fileToUpload = $path_filename_ext;
 
-    $containerName = "blockblobs" . generateRandomString();
-
-    try {
-        // Create container.
-        $blobClient->createContainer($containerName, $createContainerOptions);
-
-        // Getting local file so that we can upload it to Azure
-        // $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-        // fclose($myfile);
-
-        # Upload file as a block blob
-        echo "Uploading BlockBlob: " . PHP_EOL;
-        echo $fileToUpload;
-        echo "<br />";
-
-        // $content = fopen($fileToUpload, "r");
-
-        //Upload blob
-        // $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-        $blobClient->createBlockBlob($containerName, $fileToUpload);
-
-        // List blobs.
-        $listBlobsOptions = new ListBlobsOptions();
-        $listBlobsOptions->setPrefix("myImage");
-
-        echo "These are the blobs present in the container: ";
-
-        do {
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-            foreach ($result->getBlobs() as $blob) {
-                echo $blob->getName() . ": " . $blob->getUrl() . "<br />";
-            }
-
-            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-        } while ($result->getContinuationToken());
-        echo "<br />";
-
-        // Get blob.
-        echo "This is the content of the blob uploaded: ";
-        $blob = $blobClient->getBlob($containerName, $fileToUpload);
-        fpassthru($blob->getContentStream());
-        echo "<br />";
-    } catch (ServiceException $e) {
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code . ": " . $error_message . "<br />";
-    } catch (InvalidArgumentTypeException $e) {
-        
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code . ": " . $error_message . "<br />";
+        }
     }
 
+    // $createContainerOptions = new CreateContainerOptions();
 
-} 
+    // $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+    // // Set container metadata.
+    // $createContainerOptions->addMetaData("key1", "value1");
+    // $createContainerOptions->addMetaData("key2", "value2");
+
+    // $containerName = "blockblobs" . generateRandomString();
+
+    // try {
+    //     // Create container.
+    //     $blobClient->createContainer($containerName, $createContainerOptions);
+
+    //     // Getting local file so that we can upload it to Azure
+    //     // $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+    //     // fclose($myfile);
+
+    //     # Upload file as a block blob
+    //     echo "Uploading BlockBlob: " . PHP_EOL;
+    //     echo $fileToUpload;
+    //     echo "<br />";
+
+    //     // $content = fopen($fileToUpload, "r");
+
+    //     //Upload blob
+    //     // $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    //     $blobClient->createBlockBlob($containerName, $fileToUpload);
+
+    //     // List blobs.
+    //     $listBlobsOptions = new ListBlobsOptions();
+    //     $listBlobsOptions->setPrefix("myImage");
+
+    //     echo "These are the blobs present in the container: ";
+
+    //     do {
+    //         $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+    //         foreach ($result->getBlobs() as $blob) {
+    //             echo $blob->getName() . ": " . $blob->getUrl() . "<br />";
+    //         }
+
+    //         $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+    //     } while ($result->getContinuationToken());
+    //     echo "<br />";
+
+    //     // Get blob.
+    //     echo "This is the content of the blob uploaded: ";
+    //     $blob = $blobClient->getBlob($containerName, $fileToUpload);
+    //     fpassthru($blob->getContentStream());
+    //     echo "<br />";
+    // } catch (ServiceException $e) {
+    //     $code = $e->getCode();
+    //     $error_message = $e->getMessage();
+    //     echo $code . ": " . $error_message . "<br />";
+    // } catch (InvalidArgumentTypeException $e) {
+
+    //     $code = $e->getCode();
+    //     $error_message = $e->getMessage();
+    //     echo $code . ": " . $error_message . "<br />";
+    // }
+
+}
 
 ?>
