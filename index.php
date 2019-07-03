3cde658e98bde83abd7da01c751baf1c977d3e4f
Fitra Fadilana <fitra90@gmail.com>
2019-07-03 13:40:17 +0700

    test upload to blob

--- index.php
+++ index.php
@@ -155,66 +155,66 @@ if (isset($_POST['SubmitButton'])) { //check if form was submitted
         }
     }
 
-    // $createContainerOptions = new CreateContainerOptions();
+    $createContainerOptions = new CreateContainerOptions();
 
-    // $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
 
-    // // Set container metadata.
-    // $createContainerOptions->addMetaData("key1", "value1");
-    // $createContainerOptions->addMetaData("key2", "value2");
+    // Set container metadata.
+    $createContainerOptions->addMetaData("key1", "value1");
+    $createContainerOptions->addMetaData("key2", "value2");
 
-    // $containerName = "blockblobs" . generateRandomString();
+    $containerName = "blockblobs" . generateRandomString();
 
-    // try {
-    //     // Create container.
-    //     $blobClient->createContainer($containerName, $createContainerOptions);
+    try {
+        // Create container.
+        $blobClient->createContainer($containerName, $createContainerOptions);
 
-    //     // Getting local file so that we can upload it to Azure
-    //     // $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-    //     // fclose($myfile);
+        // Getting local file so that we can upload it to Azure
+        // $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+        // fclose($myfile);
 
-    //     # Upload file as a block blob
-    //     echo "Uploading BlockBlob: " . PHP_EOL;
-    //     echo $fileToUpload;
-    //     echo "<br />";
+        # Upload file as a block blob
+        echo "Uploading BlockBlob: " . PHP_EOL;
+        echo $fileToUpload;
+        echo "<br />";
 
-    //     // $content = fopen($fileToUpload, "r");
+        // $content = fopen($fileToUpload, "r");
 
-    //     //Upload blob
-    //     // $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-    //     $blobClient->createBlockBlob($containerName, $fileToUpload);
+        //Upload blob
+        // $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+        $blobClient->createBlockBlob($containerName, $fileToUpload);
 
-    //     // List blobs.
-    //     $listBlobsOptions = new ListBlobsOptions();
-    //     $listBlobsOptions->setPrefix("myImage");
+        // List blobs.
+        $listBlobsOptions = new ListBlobsOptions();
+        $listBlobsOptions->setPrefix("myImage");
 
-    //     echo "These are the blobs present in the container: ";
+        echo "These are the blobs present in the container: ";
 
-    //     do {
-    //         $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-    //         foreach ($result->getBlobs() as $blob) {
-    //             echo $blob->getName() . ": " . $blob->getUrl() . "<br />";
-    //         }
+        do {
+            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+            foreach ($result->getBlobs() as $blob) {
+                echo $blob->getName() . ": " . $blob->getUrl() . "<br />";
+            }
 
-    //         $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-    //     } while ($result->getContinuationToken());
-    //     echo "<br />";
+            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+        } while ($result->getContinuationToken());
+        echo "<br />";
 
-    //     // Get blob.
-    //     echo "This is the content of the blob uploaded: ";
-    //     $blob = $blobClient->getBlob($containerName, $fileToUpload);
-    //     fpassthru($blob->getContentStream());
-    //     echo "<br />";
-    // } catch (ServiceException $e) {
-    //     $code = $e->getCode();
-    //     $error_message = $e->getMessage();
-    //     echo $code . ": " . $error_message . "<br />";
-    // } catch (InvalidArgumentTypeException $e) {
+        // Get blob.
+        echo "This is the content of the blob uploaded: ";
+        $blob = $blobClient->getBlob($containerName, $fileToUpload);
+        fpassthru($blob->getContentStream());
+        echo "<br />";
+    } catch (ServiceException $e) {
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code . ": " . $error_message . "<br />";
+    } catch (InvalidArgumentTypeException $e) {
 
-    //     $code = $e->getCode();
-    //     $error_message = $e->getMessage();
-    //     echo $code . ": " . $error_message . "<br />";
-    // }
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code . ": " . $error_message . "<br />";
+    }
 
 }
 
